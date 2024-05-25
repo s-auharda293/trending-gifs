0e84b2fc6918ce7cfe75ac57be265b93562baf44
s-auharda293 <077bct036.suharda@sagarmatha.edu.np>
2024-05-25 18:54:23 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable react/prop-types */
+import "./../public/style.css";
+import Masonry from "react-masonry-css";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  return (
+    <>
+      <Heading />
+      <GifContainer />
+    </>
+  );
+};
+
+const Heading = () => {
+  return <h1 className="heading">TRENDING GIF&apos;s</h1>;
+};
+
+const GifContainer = () => {
+  const [dataFromUrl, setDataFromUrl] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.giphy.com/v1/gifs/trending?api_key="
+        );
+        let data = await response.json();
+        data = data.data.map((data) => {
+          return data.images.original.url;
+        });
+        setDataFromUrl(data);
+      } catch (error) {
+        console.error("Error fetching GIFs:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const breakpoints = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    450: 1,
+  };
+
+  return (
+    <Masonry
+      breakpointCols={breakpoints}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {dataFromUrl.map((data, index) => {
+        return <SingleGif imageSrc={data} key={data + index} />;
+      })}
+    </Masonry>
+  );
+};
+
+const SingleGif = (props) => {
+  return (
+    <>
+      <div>
+        <img src={props.imageSrc} className="image" alt="GIF" />
+      </div>
+    </>
+  );
+};
+
+export default App;
